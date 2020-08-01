a2a7412b2eac2356cb3ebbdead586387b161dff7
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-01 21:07:32 +0200

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React, { useEffect, useState } from "react";
-import moment from "moment";
-import "moment/locale/nb";
+import moment from 'moment';
+import 'moment/locale/nb';
+import React from 'react';
+import './App.css';
 
-import "./App.css";
-import useInterval from "./useInterval";
+moment.locale('nb');
 
-moment.locale("nb");
+// const CLOSING = new Date(2020, 2, 13, 16, 0, 0);
+// const OPENING = new Date(2020, 5, 15, 8, 0, 0);
 
-const CLOSING = new Date(2020, 2, 13, 16, 0, 0);
-const OPENING = new Date(2020, 5, 15, 8, 0, 0);
+// const renderDate = (inputDate) => {
+//   const date = moment(inputDate);
+//   return date.fromNow();
+// };
 
-const renderDate = (inputDate) => {
-  const date = moment(inputDate);
-  return date.fromNow();
-};
+// const IsItClosed = () => {
+//   return (
+//     <>
+//       <h2>Ja!</h2>
+//       <div>
+//         <p>Du kan fortsatt hente tingene dine på lesesalen.</p>
+//         <p>
+//           Men den stenger <strong>{renderDate(CLOSING)}</strong>.
+//         </p>
+//       </div>
+//     </>
+//   );
+// };
 
-const IsItClosed = () => {
-  return (
-    <>
-      <h2>Ja!</h2>
-      <div>
-        <p>Du kan fortsatt hente tingene dine på lesesalen.</p>
-        <p>
-          Men den stenger <strong>{renderDate(CLOSING)}</strong>.
-        </p>
-      </div>
-    </>
-  );
-};
-
-const AreWeWaiting = () => {
-  return (
-    <>
-      <h2>Nei :(</h2>
-      <p>Den åpner {renderDate(OPENING)}.</p>
-    </>
-  );
-};
+// const AreWeWaiting = () => {
+//   return (
+//     <>
+//       <h2>Nei :(</h2>
+//       <p>Den åpner {renderDate(OPENING)}.</p>
+//     </>
+//   );
+// };
 
 const WhoKnows = () => {
   return (
@@ -82,10 +80,10 @@ function App() {
         {/* {state === "OPEN" && <h2>JA :D</h2>} */}
         <Soon />
       </main>
-      <footer style={{ marginBottom: "12px" }}>
-        Laget med{" "}
+      <footer style={{ marginBottom: '12px' }}>
+        Laget med{' '}
         <span role="img" aria-label="Crying emoji">
-          😭{" "}
+          😭{' '}
         </span>
         av <a href="https://www.eons.io">Sondre Nilsen</a>
       </footer>
